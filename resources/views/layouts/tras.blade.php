af2947a503d06ae15cc29379d5740802f080dc17
tarekplmokn <plmokn.1997.vip@gmail.com>
2023-10-04 23:51:20 +0300

    test12

---
+++ resources/views/layouts/tras.blade.php
@@ -0,0 +1,1 @@
+<h1>this is tras page</h1>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/prp', function () {
+    return view('prp');
+});
+
+Route::get('/tras', function () {
+    return view('tras');
+});
+
+Route::get('auth/google', [GoogleController::class, 'signInwithGoogle']);
+Route::get('callback/google', [GoogleController::class, 'callbackToGoogle']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
